5dee8174c5efa361063c382b36774fd6bd6a1be1
claumoaesdm <claudianemoraes.m@gmail.com>
2023-10-19 21:45:35 -0300

    [e][CRUD-USUARIO] Criar serviço do Usuario na service

---
+++ src/main/java/com/projetoes/ecommerce/controller/GestaoUsuariosBean.java
@@ -0,0 +1,58 @@
+package com.projetoes.ecommerce.controller;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.projetoes.ecommerce.model.Usuario;
+import com.projetoes.ecommerce.respository.UsuarioDAO;
+
+@Named
+@SessionScoped
+public class GestaoUsuariosBean implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Inject
+	private UsuarioDAO usuarios;
+	
+	private Usuario usuario;
+	
+	private List<Usuario> listaUsuarios;
+	
+	public String detalhes(Long id) {
+		this.usuario = usuarios.porId(id);
+		return "detalheUsuario?faces-redirect=true";
+	}
+	
+	public void todosUsuarios() {
+		setListaUsuarios(usuarios.listarTodos());
+	}
+
+	public UsuarioDAO getUsuarios() {
+		return usuarios;
+	}
+
+	public void setUsuarios(UsuarioDAO usuarios) {
+		this.usuarios = usuarios;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public List<Usuario> getListaUsuarios() {
+		return listaUsuarios;
+	}
+
+	public void setListaUsuarios(List<Usuario> listaUsuarios) {
+		this.listaUsuarios = listaUsuarios;
+	}
+	
+}
