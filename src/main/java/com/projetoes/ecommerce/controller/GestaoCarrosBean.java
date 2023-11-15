6685869db39bc83cb56364dc2d089247fb44d559
José Gerlan Pereira de Paula <jgerlan@gmail.com>
2023-11-14 23:05:40 -0300

    [f] Ajuste back Carros.

--- src/main/java/com/projetoes/ecommerce/controller/GestaoCarrosBean.java
+++ src/main/java/com/projetoes/ecommerce/controller/GestaoCarrosBean.java
@@ -1,16 +1,23 @@
 package com.projetoes.ecommerce.controller;
 
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.primefaces.PrimeFaces;
+
 import com.projetoes.ecommerce.model.Carro;
+import com.projetoes.ecommerce.model.DadosCadastroVo;
 import com.projetoes.ecommerce.model.FiltroListarCarros;
 import com.projetoes.ecommerce.model.StatusCarro;
 import com.projetoes.ecommerce.respository.CarroDAO;
+import com.projetoes.ecommerce.service.CadastroCarroService;
+import com.projetoes.ecommerce.util.FacesMessages;
 
 @Named
 @SessionScoped
@@ -20,6 +27,12 @@ public class GestaoCarrosBean implements Serializable {
 
 	@Inject
 	private CarroDAO carros;
+	
+	@Inject
+	private CadastroCarroService cadastroCarroService;
+	
+	@Inject
+	private FacesMessages messages;
 
 	private Carro carro;
 
@@ -47,6 +60,96 @@ public class GestaoCarrosBean implements Serializable {
 		this.carro = new Carro();
 		filtros = new FiltroListarCarros();
 		listaCarros = carros.listarPorFiltros(filtros);
+	}
+	
+	public void prepararNovoCarro() {
+		carro = new Carro();
+	}
+
+	public void salvar() {
+		boolean carroSalvo = false;
+
+		try {
+			DadosCadastroVo dadosCadastroVo = new DadosCadastroVo();
+			dadosCadastroVo.setUsuarioIdCriacao(1);
+			dadosCadastroVo.setUsuarioIdAtualizacao(1);
+			dadosCadastroVo.setDataCriacao(new Date());
+			dadosCadastroVo.setDataAtualizacao(new Date());
+			carro.setDadosCadastro(dadosCadastroVo);
+
+			cadastroCarroService.salvar(carro);
+
+			atualizarRegistros();
+
+			carroSalvo = true;
+			PrimeFaces.current().ajax().addCallbackParam("carroSalvo", carroSalvo);
+			messages.info("Carro salvo com sucesso!");
+
+			PrimeFaces.current().ajax()
+					.update(Arrays.asList("frm-carros:carrosDataTable", "frm-carros:messages"));
+
+		} catch (Exception e) {
+			PrimeFaces.current().ajax().addCallbackParam("carroSalvo", carroSalvo);
+			messages.erro("Erro ao salvar carro!");
+		}
+	}
+
+	public void atualizar() {
+		boolean carroAtualizado = false;
+
+		try {
+			DadosCadastroVo dadosCadastroVo = carro.getDadosCadastro();
+			dadosCadastroVo.setUsuarioIdAtualizacao(1);
+			dadosCadastroVo.setDataAtualizacao(new Date());
+			carro.setDadosCadastro(dadosCadastroVo);
+
+			cadastroCarroService.atualizar(carro);
+
+			atualizarRegistros();
+
+			carroAtualizado = true;
+			PrimeFaces.current().ajax().addCallbackParam("carroAtualizado", carroAtualizado);
+			messages.info("Carro atualizado com sucesso!");
+
+			PrimeFaces.current().ajax()
+					.update(Arrays.asList("frm-carros:carrosDataTable", "frm-carros:messages"));
+
+		} catch (Exception e) {
+			PrimeFaces.current().ajax().addCallbackParam("carroAtualizado", carroAtualizado);
+			messages.erro("Erro ao atualizar carro!");
+		}
+	}
+	
+	public void excluir(Long id) {
+		cadastroCarroService.excluir(id);
+        
+        carro = null;
+        
+        atualizarRegistros();
+        
+        messages.info("Carro excluído com sucesso!");
+    }
+
+	public void prepararEdicao(Long id) {
+		carro = new Carro();
+		this.carro = carros.porId(id);
+	}
+	
+	private void atualizarRegistros() {
+		if (filtroVazio()) {
+			pesquisar();
+		} else {
+			todosCarros();
+		}
+	}
+
+	private boolean filtroVazio() {
+		return this.filtros.getMarca() != null && !"".equals(this.filtros.getMarca()) && this.filtros.getModelo() != null
+				&& !"".equals(this.filtros.getModelo()) && this.filtros.getStatus() != null
+				&& this.filtros.getValorFim() != 0 && this.filtros.getValorInicio() != 0
+				&& this.filtros.getAnoFabricacaoInicio() != null
+				&& this.filtros.getAnoFabricacaoFim() != null && this.filtros.getAnoModeloInicio() != null
+				&& this.filtros.getAnoModeloInicio() != null;
 	}
 
 	public List<Carro> getListaCarros() {
